da2c8bf269c3e8292fed6171abad011bdd150b36
EsmePutt <esme.putt@gmail.com>
2018-09-23 21:55:06 +1200

    Added staff parser

---
+++ src/Parser.java
@@ -0,0 +1,121 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+/**
+ *  A simple program designed to take a JSON file of staff members and calculate which ones are within
+ *  a certain distance of the Story Park offices.
+ * 
+ * @author Esme Putt
+ *          
+ */
+public class Parser {
+	
+	
+	int DISTANCE = 2000;
+	float EARTH_RADIUS = 6378137;
+	double OFFICE_LATITUDE = -41.2920728;
+	double OFFICE_LONGITUDE = 174.7748162;
+	String fileName = "StoryParkEmployees.json";
+	
+	public Parser (double officeLatitude, double officeLongitude, String filename) {
+		this.OFFICE_LATITUDE = officeLatitude;
+		this.OFFICE_LONGITUDE = officeLongitude;
+		this.fileName = filename;
+	}
+
+
+	/**
+	 *  Parses the JSON file and constructs a map of the employees within 2km of the StoryPark offices
+	 * 
+	 * @return employees
+	 * 			A map containing the names and distance from the StoryPark offices for all
+	 * 			employees living within 2km
+	 *          
+	 */
+	public Map<String, Double> parseEmployees() throws ParseException {
+
+		Map<String, Double> employees = new TreeMap<String, Double>();
+
+		try {
+			
+			JSONParser parser = new JSONParser();
+			JSONObject obj = (JSONObject) parser.parse(new FileReader(fileName));
+			JSONArray newArray = (JSONArray) obj.get("staff");
+
+			for (Object employee : newArray) {
+				JSONObject currentEmployee = (JSONObject) employee;
+
+				long id =  (long) currentEmployee.get("id");
+				String name = (String) currentEmployee.get("name");
+				String role = (String) currentEmployee.get("role");
+
+				JSONObject location = (JSONObject) currentEmployee.get("location");
+				double longitude = (double) location.get("longitude");
+				double latitude = (double) location.get("latitude");
+
+				double distance = calculateDistance(longitude, latitude);
+
+				if (distance < DISTANCE) {
+					employees.put(name, distance);
+				}
+
+			}
+
+		} catch (IOException e) {
+			throw new RuntimeException("file reading failed.");
+		}
+		
+		return employees;
+	}
+	
+	/**
+	 *  Calculates the distance between the employee's house and the StoryPark offices
+	 * 
+	 * @param longitude
+	 * 			The longitudinal value of the employee's house location
+	 * @param latitude
+	 * 			The latitudinal value of the employee's house location
+	 * 
+	 * @return distance
+	 * 			The distance between the employee's house and the StoryPark offices
+	 *          
+	 */
+	public double calculateDistance (double longitude, double latitude) {
+		
+		double angle = Math.acos((Math.sin(latitude) * Math.sin(OFFICE_LATITUDE)) 
+				+ (Math.cos(latitude) * Math.cos(OFFICE_LATITUDE)
+						* Math.cos(Math.abs(longitude - OFFICE_LONGITUDE))));
+		double radians = Math.toRadians(angle);
+		double distance = radians * EARTH_RADIUS;
+		return distance;
+		
+	}
+	
+	/**
+	 * Prints the names and distances of all employees within 2km of the StoryPark offices
+	 * 
+	 * @param employees
+	 * 			A map of names and distances of the employees within 2km of the StoryPark offices
+	 *          
+	 */
+	public void printEmployees(Map<String, Double> employees) {
+		
+		System.out.println("Employees within 2km of Story Park: \n");
+		
+		for (String employee : employees.keySet()) {
+			System.out.println(employee + ": " + Math.floor(employees.get(employee)) /1000 + " km from Story Park");
+		}
+		
+	}
+	
+	
+
+}
---
+++ src/ParserTests.java
@@ -0,0 +1,96 @@
+import static org.junit.Assert.assertTrue;
+
+import java.util.Map;
+
+import org.junit.Test;
+
+
+/**
+ *  A suite of tests designed to test that the Parser program can accurately calculate and store
+ *  a Map of employees within 2km of a given location. Also tests that empty and incorrectly formatted
+ *  files are handled properly.
+ * 
+ * @author Esme Putt
+ *          
+ */
+public class ParserTests {
+	
+	/**
+	 *  Tests that the Story Park JSON file is parsed correctly
+	 */
+	@Test
+	public void testValid() throws Exception {
+
+		Parser newParser = new Parser (-41.2920728, 174.7748162, "StoryParkEmployees.json");
+		Map<String, Double> employees = newParser.parseEmployees();
+		newParser.printEmployees(employees);
+		assertTrue(employees.keySet().size() == 7);
+				
+	}
+	
+	/**
+	 *  Tests that no employees are returned when the Story Park offices are moved to Auckland
+	 */
+	@Test
+	public void testDifferentLocation() throws Exception {
+		
+		Parser newParser = new Parser (-36.843397, 174.766532, "StoryParkEmployees.json");
+		Map<String, Double> employees = newParser.parseEmployees();
+		
+		assertTrue(employees.keySet().size() == 0);
+				
+	}
+	
+	/**
+	 *  Tests that an empty file is handled appropriately
+	 */
+	@Test
+	public void testEmptyFile() throws Exception {
+		boolean thrown = false;
+		try {
+			Parser newParser = new Parser (-41.2920728, 174.7748162, "Empty.json");
+			newParser.parseEmployees();
+		} catch (Exception e) {
+			thrown = true;
+		}
+		assertTrue(thrown);
+				
+	}
+	
+	/**
+	 *  Tests that a null file is handled appropriately
+	 */
+	@Test
+	public void testNullFile() throws Exception {
+
+		boolean thrown = false;
+		try {
+			Parser newParser = new Parser (-41.2920728, 174.7748162, null);
+			newParser.parseEmployees();
+		} catch (Exception e) {
+			thrown = true;
+		}
+		assertTrue(thrown);
+				
+	}
+	
+	/**
+	 *  Tests that an invalid file is handled appropriately
+	 */
+	@Test
+	public void testInvalidFile() throws Exception {
+
+		boolean thrown = false;
+		try {
+			Parser newParser = new Parser (-41.2920728, 174.7748162, "InvalidFormat.json");
+			newParser.parseEmployees();
+		} catch (Exception e) {
+			thrown = true;
+		}
+		assertTrue(thrown);
+				
+	}
+	
+	
+	
+}
